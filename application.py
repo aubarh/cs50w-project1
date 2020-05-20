3b1453537f9ab194d9c3384ef7ebcb3ca244a882
aubarh <lol@no.com>
2020-05-20 16:15:10 -0600

    app.py req.txt

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
 import os, json
-
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request, jsonify, flash
 from flask_session import Session
+import requests
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
@@ -20,10 +21,47 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    session.clear()
+
+    if request.method == "POST":    # reached route via POST
+
+        # check username
+        if not request.form.get("username"):
+            return render_template("error.html", message="Username not provided")
+
+        # check db for username
+        checkUsername = db.exectue("SELECT * FROM users WHERE username = :username",
+                {"username":request.form.get("username")}).fetchone()
+
+        # username already exists
+        if checkUsername:
+            return render_template("error.html", message="Username taken.")
+
+        # check password
+        elif  not request.form.get("password"):
+            return render_template("error.html", message="Password not provided")
+
+        # Hash password
+        pwHash = generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8)
+
+        # insert new user
+        db.execute("INSERT INTO users (username, pasword) VALUES (:username, :pawssword)",
+                {"username":request.form.get("username"),
+                    "password":pwHash})
+        db.commit()
+
+        flash('Account created', 'info')
+
+        return redirect("/login")
+
+    else:   # reached route via GET
+        return render_template("register.html")
 
 @app.route("/login")
 def login():
